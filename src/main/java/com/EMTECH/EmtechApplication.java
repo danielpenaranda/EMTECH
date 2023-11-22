a0d62bdc755d463261827641973b8fe26d50ff87
Ch4rl <Ch4rl@LAPTOP-GV845E6E>
2023-11-22 15:49:54 -0600

    Proyecto EMTECH

---
+++ src/main/java/com/EMTECH/EmtechApplication.java
@@ -0,0 +1,13 @@
+package com.EMTECH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmtechApplication.class, args);
+	}
+
+}
